76b8d5c90f198a0f05decf4c37759c5b6ea19a37
Veyzaputri <veyzaputri2004.com>
2026-09-05 20:10:44 +0700

    update database connection for Railway

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,23 @@
-<?php 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "rumah_sakit_jiwa";
+<?php
 
-$connect = new mysqli ($hostname, $username, $password, $database);
+$hostname = getenv("MYSQLHOST") ?: "localhost";
+$username = getenv("MYSQLUSER") ?: "root";
+$password = getenv("MYSQLPASSWORD") ?: "";
+$database = getenv("MYSQLDATABASE") ?: "rumah_sakit_jiwa";
+$port = getenv("MYSQLPORT") ?: 3306;
+
+$connect = new mysqli(
+    $hostname,
+    $username,
+    $password,
+    $database,
+    (int)$port
+);
+
 if ($connect->connect_error) {
-    die ('Maaf koneksi gagal :' . $connect->connect_error);
+    die("Maaf koneksi gagal: " . $connect->connect_error);
 }
+
+$connect->set_charset("utf8mb4");
+
 ?>
